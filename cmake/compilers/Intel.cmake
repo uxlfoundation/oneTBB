dbd711db45eb4f80b1286129bac87110ba1c873a
Ilya Isaev <ilya.isaev@intel.com>
2024-02-01 15:01:28 +0100

    [TCM] Add exclude-libs option for Intel compilers (#121)
    When using -static-intel option, Intel Compiler statically links internal libraries like libimf. Their symbols are exported automatically which can lead to unexpected issues due to mixed symbols in scenarios when multiple libraries with same dependencies are present. To eliminate this issue, --exclude-libs ALL link option is used.
    
    Co-authored-by: Fedotov, Aleksei <aleksei.fedotov@intel.com>

--- cmake/compilers/Intel.cmake
+++ cmake/compilers/Intel.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Intel Corporation
+# Copyright (C) 2023-2024 Intel Corporation
 #
 # This software and the related documents are Intel copyrighted materials, and your use of them is
 # governed by the express license under which they were provided to you ("License"). Unless the
@@ -13,7 +13,9 @@ if (MSVC)
     set(TCM_WARNING_LEVEL ${TCM_WARNING_LEVEL} /W3)
 else()
     include(${CMAKE_CURRENT_LIST_DIR}/GNU.cmake)
-    set(TCM_LIB_LINK_FLAGS ${TCM_LIB_LINK_FLAGS} -static-intel -Wl,-z,relro,-z,now,)
+    # "--exclude-libs,ALL" is used to avoid accidental exporting of symbols
+    #  from statically linked libraries
+    set(TCM_LIB_LINK_FLAGS ${TCM_LIB_LINK_FLAGS} -static-intel -Wl,-z,relro,-z,now,--exclude-libs,ALL)
     set(TCM_COMMON_COMPILE_FLAGS ${TCM_COMMON_COMPILE_FLAGS} -fstack-protector -Wformat -Wformat-security
                                  $<$<NOT:$<CONFIG:Debug>>:-qno-opt-report-embed -D_FORTIFY_SOURCE=2>)
     set(TCM_VISIBILITY_INLINES_HIDDEN_FLAG -fvisibility-inlines-hidden)
--- cmake/compilers/IntelLLVM.cmake
+++ cmake/compilers/IntelLLVM.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Intel Corporation
+# Copyright (C) 2023-2024 Intel Corporation
 #
 # This software and the related documents are Intel copyrighted materials, and your use of them is
 # governed by the express license under which they were provided to you ("License"). Unless the
@@ -12,4 +12,7 @@ if (WIN32)
     include(${CMAKE_CURRENT_LIST_DIR}/MSVC.cmake)
 else()
     include(${CMAKE_CURRENT_LIST_DIR}/Clang.cmake)
+    # "--exclude-libs,ALL" is used to avoid accidental exporting of symbols
+    #  from statically linked libraries
+    set(TCM_LIB_LINK_FLAGS ${TCM_LIB_LINK_FLAGS} -static-intel -Wl,--exclude-libs,ALL)
 endif()
