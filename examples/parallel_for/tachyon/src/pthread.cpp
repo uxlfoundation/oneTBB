abfa40110a1f00294478277a89c34a48af7055bd
Olga Malysheva <olga.malysheva@intel.com>
2025-03-05 05:03:24 -0600

    Disable formatting on a piece of code

--- examples/parallel_for/tachyon/src/pthread.cpp
+++ examples/parallel_for/tachyon/src/pthread.cpp
@@ -113,8 +113,10 @@ int pthread_join(pthread_t th, void **thread_return) {
 void pthread_exit(void *retval) {
     /*  specific to PTHREAD_TO_WINTHREAD  */
 
+    /* clang-format off */
     ExitThread((DWORD)(
         (std::size_t)retval)); /* thread becomes signalled so its death can be waited upon */
+    /* clang-format on */
     /*NOTREACHED*/
     assert(0);
     return; /* void fnc; can't return an error code */
