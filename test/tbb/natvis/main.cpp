946d77ec37845e2cd3ba258d937352f71b5905bd
sarathnandu <sarath.nandu.ramachandran.nair@intel.com>
2025-04-04 10:54:16 -0500

    Update copyright and set TBBROOT variable

--- test/tbb/natvis/main.cpp
+++ test/tbb/natvis/main.cpp
@@ -1,3 +1,19 @@
+/*
+    Copyright (c) 2025 Intel Corporation
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #include <iostream>
 #include "oneapi/tbb/task_arena.h"
 #include "oneapi/tbb/parallel_for.h"
@@ -15,7 +31,7 @@ int main() {
         [&](const tbb::blocked_range<size_t>& range) {
             for (size_t i = range.begin(); i != range.end(); ++i) {
                 concVec.push_back(dataStream[i]);
-				tls.local() += dataStream[i];
+                tls.local() += dataStream[i];
             }
         }
     );
--- test/tbb/natvis/test_natvis.py
+++ test/tbb/natvis/test_natvis.py
@@ -1,11 +1,31 @@
+# Copyright (c) 2025 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from vs_service import VsService
 import os
+import argparse
 
 if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Set TBBROOT and launch Visual Studio project.')
+    parser.add_argument('TBBROOT', type=str, help='Path to the TBB installation')
+    args = parser.parse_args()
+    # Set the TBBROOT environment variable
+    os.environ['TBBROOT'] = args.TBBROOT
     dirname = os.path.dirname(__file__)
     solution = os.path.join(dirname, 'natvis.sln')
-    watch_variable = "concVec"    # use Natvis
-    #file_path = r"C:\wa\TBB-Natvis\natvis\main.cpp"  # Change to your source file
+    watch_variable = "concVec"
     file_path = os.path.join(dirname, 'main.cpp')
     line_number = 33  # Change to the line where you want the breakpoint
     vs = VsService(file_path)
