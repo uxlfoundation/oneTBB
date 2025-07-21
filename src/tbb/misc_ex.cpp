85f6e01d86ce24bb7e19d2a952925803ba69b7b4
Fedotov, Aleksei <aleksei.fedotov@intel.com>
2025-07-21 13:31:07 +0200

    Implement more robust algorithm for detecting CPUs number in cgroup

--- src/tbb/misc_ex.cpp
+++ src/tbb/misc_ex.cpp
@@ -34,10 +34,9 @@
 #else
 #include <unistd.h>
 #if __unix__
-#include <thread>
-#include <fstream>
-#include <sstream>
-#include <cstring>
+#include <mntent.h>
+#include <memory>
+#include <climits>
 #if __linux__
 #include <sys/sysinfo.h>
 #endif
@@ -138,146 +137,219 @@ static int theNumProcs;
 // Linux control groups support
 class cgroup_info {
 public:
-    cgroup_info()
-        : default_num_cpus(std::thread::hardware_concurrency()), // Fallback value
-          m_num_cpus(parse_num_cpus()) {}
-
-    unsigned num_cpus() const { return m_num_cpus; }
-
-private:
-    unsigned parse_num_cpus() {
-        const std::string cgroup_line = find_cgroup_mount(mounts_path);
-        if (cgroup_line.empty())
-            return default_num_cpus; // Return default if no cgroup mount found
-
-        unsigned version = 0;
-        std::string mount_point;
-        if (!parse_version_and_mount_point(cgroup_line.c_str(), mount_point, version))
-            return default_num_cpus; // Return default if parsing fails
-
-        if (1 == version)
-            return determine_num_cpus_for_cgroup_v1(mount_point);
-        else
-            return determine_num_cpus_for_cgroup_v2(mount_point);
-    }
-
-    std::string find_cgroup_mount(const char* file_path) {
-        std::ifstream f(file_path);
-        if (!f)
-            return std::string();
-
-        std::string line;
-        while (std::getline(f, line)) {
-            if (line.length() < 6)
-                continue; // Skip empty or too short lines
-
-            if (std::strncmp(line.data(), "cgroup", 6) != 0)
-                continue; // Skip non-cgroup lines
-
-            return line;
-        }
-
-        return std::string();
-    }
-
-    bool read_line_from_file(const std::string& file_path, std::string& line) {
-        std::ifstream f(file_path);
-        if (!f || !std::getline(f, line))
-            return false; // Failed to read line
-        return true;
-    }
-
-    template <typename T>
-    bool convert_token_from_istream(std::istringstream& istream, T& value) {
-        if (!(istream >> value))
-            return false; // Failed to read period
-        return true;
-    }
-
-    bool parse_version_and_mount_point(const char* input, std::string& mount_point,
-                                       unsigned& version)
-    {
-        std::istringstream iss(input);
-        std::string device, version_str;
-        std::string* strs[] = {&device, &mount_point, &version_str};
-        for (auto& str : strs) {
-            if (!convert_token_from_istream(iss, *str))
-                return false; // Failed to read expected fields
-        }
-
-        version = 2;            // default
-        if (version_str == "cgroup")
-            version = 1;
-
-        return true;
-    }
-
-    bool parse_cpu_quota(std::istringstream& istream, unsigned long long& cpu_quota) {
-        std::string quota_str;
-        if (!convert_token_from_istream(istream, quota_str))
-            return false; // Failed to read quota
-
-        if (quota_str == "max") // CPU quota may be set to "max", which means no limit
+    static bool is_cpu_constrained(int& constrained_num_cpus) {
+        const int num = parse_cgroup_cpu_constraints();
+        if (num == error_value || num == unlimited_num_cpus)
             return false;
 
-        std::istringstream cpu_quota_stream(quota_str);
-        return convert_token_from_istream(cpu_quota_stream, cpu_quota);
+        constrained_num_cpus = num;
+        return true;
     }
 
-    bool parse_cpu_period(std::istringstream& istream, unsigned long long& cpu_period) {
-        return convert_token_from_istream(istream, cpu_period);
-    }
+private:
+    static void close_mounts_file(std::FILE *file) { endmntent(file); };
 
-    unsigned determine_num_cpus_for_cgroup_v1(const std::string& mount_point) {
-        std::string line;
-        if (!read_line_from_file(mount_point + "/cpu/cpu.cfs_quota_us", line))
-            return default_num_cpus;
+    static void close_file(std::FILE *file) { std::fclose(file); };
+    using unique_file_t = std::unique_ptr<FILE, decltype(&close_file)>;
 
-        std::istringstream iss(line);
-        unsigned long long cpu_quota = 0;
-        if (!parse_cpu_quota(iss, cpu_quota))
-            return default_num_cpus;
+    static constexpr int unlimited_num_cpus = INT_MAX;
+    static constexpr int error_value = 0; // Some impossible value for the number of CPUs
 
-        if (!read_line_from_file(mount_point + "/cpu/cpu.cfs_period_us", line))
-            return default_num_cpus;
+    //     Seems like the following algorithm makes sense (do until found):
+    // - Read the /proc/self/mounts to determine the mount point and version of the cgroup
+    // - Read the /proc/self/cgroup to determine the relative path, depending on the cgroup version.
+    // - Append the relative path with the mount point.
+    // - Depending on the cgroup version read:
+    // 	- cgroup v1: mount_point/relative-path/cpu.cfs_{quota|period}_us
+    // 	- cgroup v2: mount_point/relative-path/cpu.max
 
-        iss = std::istringstream(line);
-        unsigned long long cpu_period = 0;
-        if (!parse_cpu_period(iss, cpu_period))
-            return default_num_cpus;
-
-        return determine_num_cpus(cpu_quota, cpu_period);
-    }
-
-    unsigned determine_num_cpus_for_cgroup_v2(const std::string& mount_point) {
-        std::string line;
-        if (!read_line_from_file(mount_point + "/cpu.max", line))
-            return default_num_cpus;
-
-        std::istringstream iss(line);
-        unsigned long long cpu_quota = 0;
-        if (!parse_cpu_quota(iss, cpu_quota))
-            return default_num_cpus;
-
-        unsigned long long cpu_period = 0;
-        if (!parse_cpu_period(iss, cpu_period))
-            return default_num_cpus;
-
-        return determine_num_cpus(cpu_quota, cpu_period);
-    }
-
-    unsigned determine_num_cpus(unsigned long long cpu_quota, unsigned long long cpu_period) {
+    static int determine_num_cpus(long long cpu_quota, long long cpu_period) {
         if (0 == cpu_period)
-            return default_num_cpus; // Avoid division by zero, use the default number of CPUs
+            return error_value; // Avoid division by zero, use the default number of CPUs
 
-        const unsigned num_cpus = static_cast<unsigned>((cpu_quota + cpu_period / 2) / cpu_period);
-        return num_cpus > 0 ? num_cpus : 1;
+        const long long num_cpus = (cpu_quota + cpu_period / 2) / cpu_period;
+        return num_cpus > 0 ? int(num_cpus) : 1; // Ensure at least one CPU is returned
     }
 
-    static constexpr const char* mounts_path = "/proc/self/mounts";
-    const unsigned default_num_cpus; // Fallback value if parsing fails
+    static constexpr std::size_t rel_path_size = 256; // Size of the relative path buffer
+    struct cgroup_paths {
+        char v1_relative_path[rel_path_size] = {0};
+        char v2_relative_path[rel_path_size] = {0};
+    };
 
-    const unsigned m_num_cpus;
+    static const char* look_for_cpu_controller_path(const char* line, const char* last_char) {
+        const char* path_start = line;
+        while ((path_start = std::strstr(path_start, "cpu"))) {
+            // At least ":/" must be at the end of line for a valid cgroups file
+            if (line - path_start == 0 || last_char - path_start <= 3)
+                break; // Incorrect line in the cgroup file, skip it
+
+            const char prev_char = *(path_start - 1);
+            if (prev_char != ':' && prev_char != ',') {
+                ++path_start; // Not a valid "cpu" controller, continue searching
+                continue;
+            }
+
+            const char next_char = *(path_start + 3);
+            if (next_char != ':' && next_char != ',') {
+                ++path_start; // Not a valid "cpu" controller, continue searching
+                continue;
+            }
+
+            path_start = std::strchr(path_start + 3, ':') + 1;
+            __TBB_ASSERT(path_start <= last_char, "Too long path?");
+            break;
+        }
+        return path_start;
+    }
+
+    static void cache_relative_path_for(FILE* cgroup_fd, cgroup_paths& paths_cache) {
+        char* relative_path = nullptr;
+        char line[rel_path_size] = {0};
+        const char* last_char = line + rel_path_size - 1;
+
+        const char* path_start = nullptr;
+        while (fgets(line, rel_path_size, cgroup_fd)) {
+            path_start = nullptr;
+
+            if (std::strncmp(line, "0::", 3) == 0) {
+                path_start = line + 3; // cgroup v2 unified path
+                relative_path = paths_cache.v2_relative_path;
+            } else {
+                // cgroups v1 allows comount multiple controllers against the same hierarchy
+                path_start = look_for_cpu_controller_path(line, last_char);
+                relative_path = paths_cache.v1_relative_path;
+            }
+
+            if (path_start)
+                break;    // Found "cpu" controller path
+        }
+
+        std::strncpy(relative_path, path_start, rel_path_size);
+        relative_path[rel_path_size - 1] = '\0'; // Ensure null-termination after copy
+
+        char* new_line = std::strrchr(relative_path, '\n');
+        if (new_line)
+            *new_line = '\0';   // Ensure no new line at the end of the path is copied
+    }
+
+    static bool try_read_cgroup_v1_num_cpus_from(const char* dir, int& num_cpus) {
+        char path[PATH_MAX] = {0};
+        std::snprintf(path, PATH_MAX, "%s/cpu.cfs_quota_us", dir);
+
+        unique_file_t fd(std::fopen(path, "r"), &close_file);
+        if (!fd)
+            return false;
+
+        long long cpu_quota = 0;
+        if (std::fscanf(fd.get(), "%lld", &cpu_quota) != 1)
+            return false;
+
+        if (-1 == cpu_quota) {
+            num_cpus = unlimited_num_cpus; // -1 quota means maximum available CPUs
+            return true;
+        }
+
+        std::snprintf(path, PATH_MAX, "%s/cpu.cfs_period_us", dir);
+        fd.reset(std::fopen(path, "r"));
+        if (!fd)
+            return false;
+
+        long long cpu_period = 0;
+        if (std::fscanf(fd.get(), "%lld", &cpu_period) != 1)
+            return false;
+
+        num_cpus = determine_num_cpus(cpu_quota, cpu_period);
+        return num_cpus != error_value; // Return true if valid number of CPUs was determined
+    }
+
+    static bool try_read_cgroup_v2_num_cpus_from(const char* dir, int& num_cpus) {
+        char path[PATH_MAX] = {0};
+        std::snprintf(path, PATH_MAX, "%s/cpu.max", dir);
+
+        unique_file_t fd(fopen(path, "r"), &close_file);
+        if (!fd)
+            return false;
+
+        long long cpu_period = 0;
+        char cpu_quota_str[16] = {0};
+        if (std::fscanf(fd.get(), "%15s %lld", cpu_quota_str, &cpu_period) != 2)
+            return false;
+
+        if (std::strncmp(cpu_quota_str, "max", 3) == 0) {
+            num_cpus = unlimited_num_cpus;  // "max" means no CPU constraint
+            return true;
+        }
+
+        errno = 0; // Reset errno before strtoll
+        char* str_end = nullptr;
+        long long cpu_quota = std::strtoll(cpu_quota_str, &str_end, /*base*/ 10);
+        if (errno == ERANGE || str_end == cpu_quota_str)
+            return false;
+
+        num_cpus = determine_num_cpus(cpu_quota, cpu_period);
+        return num_cpus != error_value; // Return true if valid number of CPUs was determined
+    }
+
+    static int parse_cgroup_entry(const char* mnt_dir, const char* mnt_type, FILE* cgroup_fd,
+                                  cgroup_paths& paths_cache)
+    {
+        int num_cpus = error_value; // Initialize to an impossible value
+        char dir[PATH_MAX] = {0};
+        if (!std::strncmp(mnt_type, "cgroup2", 7)) { // Found cgroup v2 mount entry
+            // At first, try reading CPU quota directly
+            if (try_read_cgroup_v2_num_cpus_from(mnt_dir, num_cpus))
+                return num_cpus; // Successfully read number of CPUs for cgroup v2
+
+            if (!*paths_cache.v2_relative_path)
+                cache_relative_path_for(cgroup_fd, paths_cache);
+
+            // Now try reading including relative path
+            std::snprintf(dir, PATH_MAX, "%s/%s", mnt_dir, paths_cache.v2_relative_path);
+            try_read_cgroup_v2_num_cpus_from(dir, num_cpus);
+            return num_cpus;
+        }
+
+        __TBB_ASSERT(std::strncmp(mnt_type, "cgroup", 6) == 0, "Unexpected cgroup type");
+
+        if (try_read_cgroup_v1_num_cpus_from(mnt_dir, num_cpus))
+            return num_cpus; // Successfully read number of CPUs for cgroup v1
+
+        if (!*paths_cache.v1_relative_path)
+            cache_relative_path_for(cgroup_fd, paths_cache);
+
+        std::snprintf(dir, PATH_MAX, "%s/%s", mnt_dir, paths_cache.v1_relative_path);
+        try_read_cgroup_v1_num_cpus_from(dir, num_cpus);
+        return num_cpus;
+    }
+
+    static int parse_cgroup_cpu_constraints() {
+        using unique_mounts_file_t = std::unique_ptr<FILE, decltype(&close_mounts_file)>;
+        using unique_file_t = std::unique_ptr<FILE, decltype(&close_file)>;
+
+        // Reading /proc/self/mounts and /proc/self/cgroup anyway, so open them right away
+        unique_mounts_file_t mounts_file_ptr(setmntent("/proc/self/mounts", "r"), &close_mounts_file);
+        unique_file_t cgroup_file_ptr(fopen("/proc/self/cgroup", "r"), &close_file);
+        if (!mounts_file_ptr || !cgroup_file_ptr)
+            return error_value;
+
+        cgroup_paths relative_paths_cache;
+        struct mntent mntent;
+        const std::size_t buffer_size = 4 * 4096; // TODO: Leave a comment why using such size
+        char mount_entry_buffer[buffer_size];
+
+        int found_num_cpus = error_value; // Initialize to an impossible value
+        // Read the mounts file and cgroup file to determine the number of CPUs
+        while (getmntent_r(mounts_file_ptr.get(), &mntent, mount_entry_buffer, buffer_size)) {
+            if (std::strncmp(mntent.mnt_type, "cgroup", 6) == 0) {
+                found_num_cpus = parse_cgroup_entry(mntent.mnt_dir, mntent.mnt_type,
+                                                    cgroup_file_ptr.get(), relative_paths_cache);
+                if (found_num_cpus != error_value)
+                    break;
+            }
+        }
+        return found_num_cpus;
+    }
 };
 
 static void initialize_hardware_concurrency_info () {
@@ -340,9 +412,13 @@ static void initialize_hardware_concurrency_info () {
         availableProcs = (maxProcs == INT_MAX) ? sysconf(_SC_NPROCESSORS_ONLN) : maxProcs;
         delete[] processMask;
     }
-    cgroup_info ci;
-    unsigned num_procs = availableProcs > 0 ? availableProcs : 1; // Fail safety strap
-    theNumProcs = min(ci.num_cpus(), num_procs);
+    int num_procs = availableProcs > 0 ? availableProcs : 1; // Fail safety strap
+    int cgroup_num_cpus = INT_MAX;
+    if (cgroup_info::is_cpu_constrained(cgroup_num_cpus)) {
+        // If cgroup is used, limit the number of processors to the constrained value.
+        num_procs = std::min(num_procs, cgroup_num_cpus);
+    }
+    theNumProcs = num_procs;
     __TBB_ASSERT( theNumProcs <= sysconf(_SC_NPROCESSORS_ONLN), nullptr);
 }
 
