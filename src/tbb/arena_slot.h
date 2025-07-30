e35c2c2cc3c0998007c6c13f514f20f14a2e838e
Alexey Kukanov <alexey.kukanov@intel.com>
2025-07-30 14:49:03 +0200

    Fix a bug found during code review

--- src/tbb/arena_slot.h
+++ src/tbb/arena_slot.h
@@ -191,7 +191,7 @@ public:
             hd = head.load(std::memory_order_relaxed);
             tl = tail.load(std::memory_order_relaxed);
             unlock_task_pool(the_task_pool);
-            if ( (std::intptr_t)hd > (std::intptr_t)tl || ( hd == tl && !tail_unstable ) ) {
+            if ( (std::intptr_t)hd >= (std::intptr_t)tl && !tail_unstable ) {
                 return false;
             }
         }
