30da373adac9042e04d143a8c4d6b438ff648343
Alexey Kukanov <alexey.kukanov@intel.com>
2025-08-01 23:54:27 +0200

    Fix a silly mistake

--- src/tbb/scheduler_common.h
+++ src/tbb/scheduler_common.h
@@ -410,7 +410,7 @@ public:
     d1::task* get_critical_task(d1::task*, execution_data_ext&, isolation_type, bool);
 
     template <bool ITTPossible, typename Waiter>
-    d1::task* receive_or_steal_task(thread_data& tls, execution_data_ext& ed, Waiter& waiter,
+    d1::task* receive_or_steal_task(thread_data& tls, execution_data_ext& ed, Waiter& waiter, context_guard_helper& ctxguard,
                                 isolation_type isolation, bool outermost, bool criticality_absence);
 
     template <bool ITTPossible, typename Waiter>
