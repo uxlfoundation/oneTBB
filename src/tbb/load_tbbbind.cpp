106b875d7f11b405f5952f94daf5bce0ccf3d188
Alexandr Konovalov <alexandr.konovalov@intel.com>
2025-08-26 15:42:33 +0200

    Update copyright.

--- src/tbb/load_tbbbind.cpp
+++ src/tbb/load_tbbbind.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2019-2025 Intel Corporation
+    Copyright (c) 2005-2025 Intel Corporation
     Copyright (c) 2025 UXL Foundation Contributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
