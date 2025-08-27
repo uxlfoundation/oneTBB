71e49b7d06809868ba3ee9de6e30d6b9c6fd53da
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2025-08-27 18:33:20 +0300

    Enable TTAS strategy for spin_mutex and queuing_mutex

--- include/oneapi/tbb/spin_mutex.h
+++ include/oneapi/tbb/spin_mutex.h
@@ -68,14 +68,18 @@ public:
     void lock() {
         atomic_backoff backoff;
         call_itt_notify(prepare, this);
-        while (m_flag.exchange(true)) backoff.pause();
+
+        while (m_flag.load(std::memory_order_relaxed) || m_flag.exchange(true)) {
+            backoff.pause();
+        }
         call_itt_notify(acquired, this);
     }
 
     //! Try acquiring lock (non-blocking)
     /** Return true if lock acquired; false otherwise. */
     bool try_lock() {
-        bool result = !m_flag.exchange(true);
+        bool result = !m_flag.load(std::memory_order_relaxed) && !m_flag.exchange(true);
+
         if (result) {
             call_itt_notify(acquired, this);
         }
@@ -131,4 +135,3 @@ inline namespace v1 {
 }
 
 #endif /* __TBB_spin_mutex_H */
-
