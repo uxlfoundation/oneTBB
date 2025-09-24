8e5f1a5e61870b51cc4e4828c60d647eeab2ee38
Mike Voss <michaelj.voss@intel.com>
2025-09-24 15:19:54 -0500

    Added starvation test

--- test/tbb/test_join_node.cpp
+++ test/tbb/test_join_node.cpp
@@ -1,5 +1,6 @@
 /*
-    Copyright (c) 2005-2024 Intel Corporation
+    Copyright (c) 2005-2025 Intel Corporation
+    Copyright (c) 2025 UXL Foundation Contributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -23,6 +24,7 @@
 
 #include "test_join_node.h"
 #include "common/test_join_node_multiple_predecessors.h"
+#include <memory>
 
 //! \file test_join_node.cpp
 //! \brief Test for [flow_graph.join_node] specification
@@ -190,4 +192,97 @@ TEST_CASE("Test reservation on the port") {
 
     result = port1.reserve(value);
     CHECK_MESSAGE(result, "incorrect reserve return value");
+}
+
+//! \brief \ref error_guessing
+TEST_CASE("Test reservation fairness") {
+    const int N = 100; // number of messages to be sent to each input
+    const int THRESHOLD = N/2; // max difference between the number of acquisitions and 2*inputs
+    tbb::flow::graph g;
+
+    using big_object = int;
+    using big_msg_type = std::shared_ptr<big_object>;
+    using resource_type = int;
+
+    tbb::flow::broadcast_node<big_msg_type> source(g);
+    tbb::flow::queue_node<big_msg_type> b1(g), b2(g), b3(g);
+    tbb::flow::buffer_node<resource_type> r1(g), r2(g);
+    std::atomic<int> r1_count{0}, r2_count{0};
+    bool threshold_exceeded1 = false, threshold_exceeded2 = false, threshold_exceeded3 = false;
+
+    using join_2_type = tbb::flow::join_node<std::tuple<big_msg_type, resource_type>, tbb::flow::reserving>;
+    using join_3_type = tbb::flow::join_node<std::tuple<big_msg_type, resource_type, resource_type>, tbb::flow::reserving>;
+    join_2_type j1(g), j2(g);
+    join_3_type j3(g);
+
+    // Helper to create lambda bodies for single-resource nodes
+    auto make_single_lambda = [&](std::atomic<int>& counter, bool& threshold_flag) {
+        return [&](const join_2_type::output_type &in) {
+            auto acquisition_number = ++counter;
+            auto msg = std::get<0>(in);
+            if (acquisition_number - 2*(*msg) > THRESHOLD) {
+                threshold_flag = true;
+            }
+            utils::Sleep(1);
+            return 1;
+        };
+    };
+
+    tbb::flow::function_node<join_2_type::output_type, int>
+    f1(g, tbb::flow::serial, make_single_lambda(r1_count, threshold_exceeded1));
+
+    tbb::flow::function_node<join_2_type::output_type, int>
+    f2(g, tbb::flow::serial, make_single_lambda(r2_count, threshold_exceeded2));
+
+    tbb::flow::function_node<join_3_type::output_type, int>
+    f3(g, tbb::flow::serial, [&](const join_3_type::output_type &in) {
+        auto r1_acquisition_number = ++r1_count;
+        auto r2_acquisition_number = ++r2_count;
+        auto msg = std::get<0>(in);
+        if ((r1_acquisition_number - 2*(*msg) > THRESHOLD)
+            || (r2_acquisition_number - 2*(*msg) > THRESHOLD)) {
+            threshold_exceeded3 = true;
+        }
+        utils::Sleep(1);
+        return 1;
+    });
+
+    r1_count = 0;
+    r2_count = 0;
+
+    tbb::flow::make_edge(source, b1);
+    tbb::flow::make_edge(source, b2);
+    tbb::flow::make_edge(source, b3);
+
+    tbb::flow::make_edge(b1, tbb::flow::input_port<0>(j1));
+    tbb::flow::make_edge(r1, tbb::flow::input_port<1>(j1));
+    tbb::flow::make_edge(j1, f1);
+    tbb::flow::make_edge(f1, r1);
+
+    tbb::flow::make_edge(b2, tbb::flow::input_port<0>(j2));
+    tbb::flow::make_edge(r2, tbb::flow::input_port<1>(j2));
+    tbb::flow::make_edge(j2, f2);
+    tbb::flow::make_edge(f2, r2);
+
+    tbb::flow::make_edge(b3, tbb::flow::input_port<0>(j3));
+    tbb::flow::make_edge(r1, tbb::flow::input_port<1>(j3));
+    tbb::flow::make_edge(r2, tbb::flow::input_port<2>(j3));
+    tbb::flow::make_edge(j3, f3);
+    tbb::flow::make_edge(f3, r1);
+    tbb::flow::make_edge(f3, r2);
+
+    r1.try_put(1);
+    r2.try_put(1);
+
+    for (int i = 0; i < N; ++i) {
+        source.try_put(big_msg_type(new big_object{i}));
+        utils::Sleep(1);
+    }
+    g.wait_for_all();
+
+    CHECK_MESSAGE(r1_count.load() == 2 * N, "Incorrect number of acquisitions of r1");
+    CHECK_MESSAGE(r2_count.load() == 2 * N, "Incorrect number of acquisitions of r2");
+    CHECK_MESSAGE(!threshold_exceeded1, "Possible starvation of f1");
+    CHECK_MESSAGE(!threshold_exceeded2, "Possible starvation of f2");
+    CHECK_MESSAGE(!threshold_exceeded3, "Possible starvation of f3");
 }
