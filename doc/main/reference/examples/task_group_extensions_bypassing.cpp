19d54d6e4ab18848e18298792b04cab8753c071b
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2025-10-10 14:18:52 +0300

    Fix typo

--- doc/main/reference/examples/task_group_extensions_bypassing.cpp
+++ doc/main/reference/examples/task_group_extensions_bypassing.cpp
@@ -59,7 +59,7 @@ template <typename RandomAccessIterator, typename Function>
 void parallel_for(RandomAccessIterator begin, RandomAccessIterator end, Function f) {
     tbb::task_group tg;
     // Run the root task
-    tg,run_and_wait(for_task<RandomAccessIterator, Function>{begin, end, std::move(f), tg});
+    tg.run_and_wait(for_task<RandomAccessIterator, Function>{begin, end, std::move(f), tg});
 }
 /*end_task_group_extensions_bypassing_example*/
 
