034f5eaefb3cc36e11a18b5215a9c51b21521f73
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2026-01-09 14:22:25 +0200

    Fix copyright

--- test/conformance/conformance_blocked_nd_range.cpp
+++ test/conformance/conformance_blocked_nd_range.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2017-2025 Intel Corporation
+    Copyright (c) 2017-2024 Intel Corporation
     Copyright (c) 2026 UXL Foundation Contributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
