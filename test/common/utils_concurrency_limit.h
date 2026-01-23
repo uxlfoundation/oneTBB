33caaac4f8cce4130df474d93a787b6b8aa24093
Ganael Laplanche <ganael.laplanche@martymac.org>
2026-01-23 12:59:22 +0100

    Update test/common/utils_concurrency_limit.h
    Co-authored-by: Konstantin Boyarinov <konstantin.boyarinov@intel.com>

--- test/common/utils_concurrency_limit.h
+++ test/common/utils_concurrency_limit.h
@@ -1,6 +1,6 @@
 /*
     Copyright (c) 2020-2025 Intel Corporation
-    Copyright (c) 2025 UXL Foundation Сontributors
+    Copyright (c) 2025-2026 UXL Foundation Сontributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
