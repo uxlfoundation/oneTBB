f2c04ed9b6d2babb84b15a3618d65994f7be6bf8
Mike Voss <michaelj.voss@intel.com>
2026-03-06 15:55:58 -0600

    Fixed starvation test to use resource_limiter

--- test/tbb/test_resource_limited_node.cpp
+++ test/tbb/test_resource_limited_node.cpp
@@ -404,16 +404,16 @@ TEST_CASE("Test reservation fairness") {
     broadcast_node<big_msg_type> source(g);
     queue_node<big_msg_type> b1(g), b2(g), b3(g);
 
-    // Create resource providers (replaces buffer_nodes)
-    resource_provider<resource_type> r1_provider(1);
-    resource_provider<resource_type> r2_provider(1);
+    // Create resource limiters (replaces buffer_nodes)
+    resource_limiter<resource_type> r1_limiter(1);
+    resource_limiter<resource_type> r2_limiter(1);
 
     std::atomic<int> r1_count{0}, r2_count{0};
     bool threshold_exceeded1 = false, threshold_exceeded2 = false, threshold_exceeded3 = false;
 
-    // Path 1: Requires r1 only
-    using node_type_r1 = resource_limited_node<big_msg_type, std::tuple<>>;
-    node_type_r1 consumer1(g, serial, std::tie(r1_provider),
+    // Node 1: Requires r1 only
+    using node_type = resource_limited_node<big_msg_type, std::tuple<>>;
+    node_type n1(g, serial, std::tie(r1_limiter),
         [&](const big_msg_type& msg, auto&, resource_type&) {
             auto acquisition_number = ++r1_count;
             if (acquisition_number - 2*(*msg) > THRESHOLD) {
@@ -422,9 +422,8 @@ TEST_CASE("Test reservation fairness") {
             utils::Sleep(1);
         });
 
-    // Path 2: Requires r2 only
-    using node_type_r2 = resource_limited_node<big_msg_type, std::tuple<>>;
-    node_type_r2 path2_node(g, serial, std::tie(r2_provider),
+    // Node 2: Requires r2 only
+    node_type n2(g, serial, std::tie(r2_limiter),
         [&](const big_msg_type& msg, auto&, resource_type&) {
             auto acquisition_number = ++r2_count;
             if (acquisition_number - 2*(*msg) > THRESHOLD) {
@@ -433,9 +432,8 @@ TEST_CASE("Test reservation fairness") {
             utils::Sleep(1);
         });
 
-    // Path 3: Requires both r1 and r2
-    using node_type_both = resource_limited_node<big_msg_type, std::tuple<>>;
-    node_type_both path3_node(g, serial, std::tie(r1_provider, r2_provider),
+    // Node 3: Requires both r1 and r2
+    node_type n3(g, serial, std::tie(r1_limiter, r2_limiter),
         [&](const big_msg_type& msg, auto&, resource_type&, resource_type&) {
             auto r1_acquisition_number = ++r1_count;
             auto r2_acquisition_number = ++r2_count;
@@ -454,9 +452,9 @@ TEST_CASE("Test reservation fairness") {
     make_edge(source, b2);
     make_edge(source, b3);
 
-    make_edge(b1, path1_node);
-    make_edge(b2, path2_node);
-    make_edge(b3, path3_node);
+    make_edge(b1, n1);
+    make_edge(b2, n2);
+    make_edge(b3, n3);
 
     for (int i = 0; i < N; ++i) {
         source.try_put(big_msg_type(new big_object{i}));
