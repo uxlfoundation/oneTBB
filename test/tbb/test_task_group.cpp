e5f97af5913fd05995203598a701739b8ef77b56
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2026-03-09 14:01:59 +0200

    Add test concurrent wait

--- test/tbb/test_task_group.cpp
+++ test/tbb/test_task_group.cpp
@@ -2401,12 +2401,53 @@ void test_single_task_wait_transferring(bool do_cancellation, wait_for_function_
     }
 }
 
+void test_concurrent_singe_task_wait(bool do_cancellation, wait_for_function_tag tag) {
+    tbb::task_group tg;
+    tbb::task_arena arena;
+
+    tbb::task_handle task;
+    tbb::task_completion_handle comp_handle;
+    
+    arena.execute([&] {
+        task = tg.defer([&] {
+            CHECK_MESSAGE(do_cancellation, "Task should not be called in case of group cancellation");
+        });
+        comp_handle = task;
+    });
+
+    tbb::task_group_status expected_status = do_cancellation ? tbb::task_group_status::canceled
+                                                             : tbb::task_group_status::task_complete;
+
+    int num_threads = tbb::this_task_arena::max_concurrency();
+    utils::SpinBarrier barrier(num_threads);
+
+    utils::NativeParallelFor(0, num_threads,
+        [&](std::size_t index) {
+            tbb::task_group_status status;
+            barrier.wait();
+            if (index == 0) {
+                // Short sleep to increase the number of threads entered the wait_for function
+                utils::Sleep(10);
+                
+                if (do_cancellation) {
+                    tg.cancel();
+                }
+
+                status = run_and_wait_for(std::move(task), tg, arena, tag);
+            } else {
+                status = wait_for(comp_handle, tg, arena, tag);
+            }
+            CHECK_MESSAGE(status == expected_status, "Incorrect status returned");
+        });
+}
+
 void test_single_task_wait_base(bool cancellation, wait_for_function_tag tag) {
     for (unsigned num_threads = MinThread; num_threads <= MaxThread; ++num_threads) {
         tbb::global_control ctl(tbb::global_control::max_allowed_parallelism, num_threads);
         test_single_task_wait_with_no_dependencies(cancellation, tag);
         test_single_task_wait_with_dependencies(cancellation, tag);
         test_single_task_wait_transferring(cancellation, tag);
+        test_concurrent_singe_task_wait(cancellation, tag);
     }
 }
 
@@ -2529,6 +2570,7 @@ TEST_CASE("test single task wait") {
     test_single_task_wait(/*cancel = */true);
 }
 
+//! \brief \ref interface \ref requirement
 TEST_CASE("test task_group::get_status_of") {
     test_get_status_of();
 }
