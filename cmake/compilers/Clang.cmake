9335e290bb9d95aae31a36c658c2bb0075069baf
Alexandr-Konovalov <alexandr.konovalov@intel.com>
2026-03-17 15:33:40 +0100

    Fix typos.
    Co-authored-by: Dmitri Mokhov <dmitri.n.mokhov@intel.com>

--- cmake/compilers/Clang.cmake
+++ cmake/compilers/Clang.cmake
@@ -80,8 +80,8 @@ set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS}
 if (NOT APPLE AND NOT ANDROID_PLATFORM AND CMAKE_SYSTEM_PROCESSOR MATCHES "(AMD64|amd64|i.86|x86)" AND NOT WIN32)
     set(TBB_LIB_COMPILE_FLAGS ${TBB_LIB_COMPILE_FLAGS} -fstack-clash-protection)
     if (NOT CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
-        # Some versions of Clang implitily set -march=i686 when compiling for x86 and some don't.
-        # -fcf-protection requires i686, so check -fcf-protection explititely.
+        # Some versions of Clang implicitly set -march=i686 when compiling for x86 and some don't.
+        # -fcf-protection requires i686, so check -fcf-protection explicitly.
         include(CheckCXXSourceCompiles)
         set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
         set(CMAKE_REQUIRED_FLAGS "-fcf-protection=full")
