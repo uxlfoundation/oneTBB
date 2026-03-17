d851d7b61940b5e1b5b1562a74ac0c68daf96e36
Alexandr-Konovalov <alexandr.konovalov@intel.com>
2026-03-17 15:39:42 +0100

    Use EMSCRIPTEN
    Co-authored-by: Dmitri Mokhov <dmitri.n.mokhov@intel.com>

--- cmake/compilers/Clang.cmake
+++ cmake/compilers/Clang.cmake
@@ -79,7 +79,7 @@ set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS}
 
 if (NOT APPLE AND NOT ANDROID_PLATFORM AND CMAKE_SYSTEM_PROCESSOR MATCHES "(AMD64|amd64|i.86|x86)" AND NOT WIN32)
     set(TBB_LIB_COMPILE_FLAGS ${TBB_LIB_COMPILE_FLAGS} -fstack-clash-protection)
-    if (NOT CMAKE_SYSTEM_NAME STREQUAL "Emscripten")
+    if (NOT EMSCRIPTEN)
         # Some versions of Clang implicitly set -march=i686 when compiling for x86 and some don't.
         # -fcf-protection requires i686, so check -fcf-protection explicitly.
         include(CheckCXXSourceCompiles)
