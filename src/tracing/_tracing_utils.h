9341ed953d9b6630b823ddc790d63d9eca3b6c80
Aleksei Fedotov <aleksei.fedotov@intel.com>
2026-04-17 17:22:23 +0200

    [TCM] Update license to Apache 2.0 WITH LLVM-exception (#228)

--- src/tracing/_tracing_utils.h
+++ src/tracing/_tracing_utils.h
@@ -1,13 +1,7 @@
 /*
-    Copyright (C) 2024-2026 Intel Corporation
+   Copyright (c) 2024 Intel Corporation
 
-    This software and the related documents are Intel copyrighted materials, and your use of them is
-    governed by the express license under which they were provided to you ("License"). Unless the
-    License provides otherwise, you may not use, modify, copy, publish, distribute, disclose or
-    transmit this software or the related documents without Intel's prior written permission.
-
-    This software and the related documents are provided as is, with no express or implied
-    warranties, other than those that are expressly stated in the License.
+   SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 */
 
 #ifndef __TCM_TRACING_UTILS_HEADER
