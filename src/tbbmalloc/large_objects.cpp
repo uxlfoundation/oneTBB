bd694791dbd23732a4d07c664eee71bb39d2e0be
Fedotov, Aleksei <aleksei.fedotov@intel.com>
2026-05-18 12:32:48 +0200

    Add assert for Props::sizeToIdx(size) function

--- src/tbbmalloc/large_objects.cpp
+++ src/tbbmalloc/large_objects.cpp
@@ -844,7 +844,8 @@ void LargeObjectCache::reset()
 template<typename Props>
 LargeMemoryBlock *LargeObjectCacheImpl<Props>::get(ExtMemoryPool *extMemoryPool, size_t size)
 {
-    unsigned idx = Props::sizeToIdx(size);
+    const unsigned idx = Props::sizeToIdx(size);
+    MALLOC_ASSERT(idx < numBins, "Array out of bounds access found.");
 
     LargeMemoryBlock *lmb = bin[idx].get(extMemoryPool, size, &bitMask, idx);
 
