86d15a1995564e837bb0b97792df49c072364e59
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2026-06-09 13:03:09 +0200

    Prepare test for profiling

--- include/oneapi/tbb/test_parallel_quick_sort.cpp
+++ include/oneapi/tbb/test_parallel_quick_sort.cpp
@@ -35,32 +35,32 @@ int main() {
 
     tbb::tick_count finish_parallel = tbb::tick_count::now();
 
-    tbb::tick_count start_serial = tbb::tick_count::now();
+//     tbb::tick_count start_serial = tbb::tick_count::now();
 
-    std::sort(v_copy.begin(), v_copy.end(), std::less<int>{});
+//     std::sort(v_copy.begin(), v_copy.end(), std::less<int>{});
 
-    tbb::tick_count finish_serial = tbb::tick_count::now();
+//     tbb::tick_count finish_serial = tbb::tick_count::now();
 
-    tbb::tick_count start_psort = tbb::tick_count::now();
+//     tbb::tick_count start_psort = tbb::tick_count::now();
 
-    tbb::parallel_sort(v_copy2.begin(), v_copy2.end(), std::less<int>{});
+//     tbb::parallel_sort(v_copy2.begin(), v_copy2.end(), std::less<int>{});
 
-    tbb::tick_count finish_psort = tbb::tick_count::now();
+//     tbb::tick_count finish_psort = tbb::tick_count::now();
 
-#if TEST_DPL
-    tbb::tick_count start_dpl = tbb::tick_count::now();
+// #if TEST_DPL
+//     tbb::tick_count start_dpl = tbb::tick_count::now();
 
-    oneapi::dpl::sort(oneapi::dpl::execution::par, v_copy3.begin(), v_copy3.end(), std::less<int>{});
+//     oneapi::dpl::sort(oneapi::dpl::execution::par, v_copy3.begin(), v_copy3.end(), std::less<int>{});
 
-    tbb::tick_count finish_dpl = tbb::tick_count::now();
-#endif
+//     tbb::tick_count finish_dpl = tbb::tick_count::now();
+// #endif
 
     std::cout << "Elapsed time (parallel): " << (finish_parallel - start_parallel).seconds() << std::endl;
-    std::cout << "Elapsed time (TBB): " << (finish_psort - start_psort).seconds() << std::endl;
-#if TEST_DPL
-    std::cout << "Elapsed time (DPL): " << (finish_dpl - start_dpl).seconds() << std::endl;
-#endif
-    std::cout << "Elapsed time (serial) " << (finish_serial - start_serial).seconds() << std::endl;
+    // std::cout << "Elapsed time (TBB): " << (finish_psort - start_psort).seconds() << std::endl;
+// #if TEST_DPL
+//     std::cout << "Elapsed time (DPL): " << (finish_dpl - start_dpl).seconds() << std::endl;
+// #endif
+//     std::cout << "Elapsed time (serial) " << (finish_serial - start_serial).seconds() << std::endl;
 
     std::cout << "parallel sorted: " << std::is_sorted(v.begin(), v.end()) << std::endl;
     std::cout << "matches serial:  " << (v == v_copy) << std::endl;
