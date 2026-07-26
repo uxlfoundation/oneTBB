e32320893dcdb5be9249746052f479ffde0ecbfd
Isaev, Ilya <ilya.isaev@intel.com>
2026-07-26 15:40:31 +0200

    Use more tries
    Signed-off-by: Isaev, Ilya <ilya.isaev@intel.com>

--- src/tbb/arena_slot.cpp
+++ src/tbb/arena_slot.cpp
@@ -146,10 +146,13 @@ d1::task* arena_slot::get_task(execution_data_ext& ed, isolation_type isolation)
     return result;
 }
 
+<<<<<<< HEAD
 d1::task* arena_slot::steal_task(arena& a, isolation_type isolation, std::size_t slot_index) {
     constexpr int max_lock_attempts = 4;
+=======
+d1::task* arena_slot::steal_task(arena& a, isolation_type isolation, std::size_t slot_index, steal_attempt_outcome& outcome) {
+>>>>>>> 54202772 (Use more tries)
     d1::task** victim_pool = nullptr;
-    int attempts = 0;
     atomic_backoff backoff;
     do {
         victim_pool = try_lock_task_pool();
@@ -158,8 +161,7 @@ d1::task* arena_slot::steal_task(arena& a, isolation_type isolation, std::size_t
             break;
         }
         __TBB_ASSERT(victim_pool == LockedTaskPool, nullptr);
-        attempts++;
-    } while (attempts < max_lock_attempts && backoff.bounded_pause() );
+    } while (backoff.bounded_pause());
 
     if (victim_pool == LockedTaskPool || victim_pool == EmptyTaskPool) {
         return nullptr;
