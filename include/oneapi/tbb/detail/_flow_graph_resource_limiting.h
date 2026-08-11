2159a84c51845d995137ec3cd8ccdedb5461e29f
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2026-08-11 18:49:40 +0200

    Rewrite the assert

--- include/oneapi/tbb/detail/_flow_graph_resource_limiting.h
+++ include/oneapi/tbb/detail/_flow_graph_resource_limiting.h
@@ -162,8 +162,7 @@ public:
     resource_limiter(InputIterator first, InputIterator last)
         : m_resource_handles(first, last)
     {
-        __TBB_ASSERT(std::distance(m_resource_handles.begin(), m_resource_handles.end()),
-                     "Attempt to create a resource_limiter with 0 resource handles");
+        __TBB_ASSERT(!m_resource_handles.empty(), "Attempt to create a resource_limiter with 0 resource handles");
     }
 
     template <typename ContainerBasedSequence,
