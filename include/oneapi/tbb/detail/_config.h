9b767b7c9bd3c48a2763b14fef81ecc443447e00
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2026-09-08 11:58:22 +0200

    Fix memory resource feature detection on Clang

--- include/oneapi/tbb/detail/_config.h
+++ include/oneapi/tbb/detail/_config.h
@@ -263,8 +263,8 @@
 // GCC4.8 on RHEL7 does not support std::is_trivially_copyable
 #define __TBB_CPP11_TYPE_PROPERTIES_PRESENT             (_LIBCPP_VERSION || _MSC_VER >= 1700 || (__TBB_GLIBCXX_VERSION >= 50000 && __GXX_EXPERIMENTAL_CXX0X__))
 
-#define __TBB_CPP17_MEMORY_RESOURCE_PRESENT             (_MSC_VER >= 1913 && (__TBB_LANG > 201402L) || \
-                                                        __TBB_GLIBCXX_VERSION >= 90000 && __TBB_LANG >= 201703L)
+#define __TBB_CPP17_MEMORY_RESOURCE_PRESENT             ((_MSC_VER >= 1913 && __TBB_LANG > 201402L) || \
+                                                         (__TBB_LANG >= 201703L && (__TBB_GLIBCXX_VERSION >= 90000 || _LIBCPP_VERSION >= 160000)))
 #define __TBB_CPP17_HW_INTERFERENCE_SIZE_PRESENT        (_MSC_VER >= 1911)
 #define __TBB_CPP17_LOGICAL_OPERATIONS_PRESENT          (__TBB_LANG >= 201703L)
 #define __TBB_CPP17_ALLOCATOR_IS_ALWAYS_EQUAL_PRESENT   (__TBB_LANG >= 201703L)
